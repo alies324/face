f0788953f041ba598fbe4a08f6e0b521aa054211
alies324 <sanaz136674@gmail.com>
2016-02-25 10:21:13 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "set",
     "get",
     "broadcast",
-    "download_media",
+    "download_media",sudo_users={120647760}--sudo users
     "invite",
     "all",
     "leave_ban",
